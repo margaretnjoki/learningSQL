457082071d6b873c3d8b3887b299a0fb50c3d33a
margaretnjoki <maggykiroko@gmail.com>
2026-07-05 19:16:00 +0300

    All order lines as: customer name, book title, quantity

--- day3_joins.sql
+++ day3_joins.sql
@@ -20,14 +20,23 @@
 -- FROM customers c
 -- JOIN orders ON c.id = orders.customer_id
 --  WHERE NOT EXISTS (SELECT 1 from orders WHERE orders.id = customer_id )
+--
+-- SELECT c.name
+-- FROM customers c
+-- WHERE NOT EXISTS (
+--     SELECT 1
+--     FROM orders o
+--     WHERE o.customer_id = c.id
+-- );
 
-SELECT c.name
+SELECT c.name, b.title, oi.quantity
 FROM customers c
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM orders o
-    WHERE o.customer_id = c.id
-);
+         JOIN orders o       ON o.customer_id = c.id
+         JOIN order_items oi ON oi.order_id   = o.id
+         JOIN books b        ON b.id          = oi.book_id;
+
+
+
 
 
 
