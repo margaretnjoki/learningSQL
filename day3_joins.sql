915c7653c30d3b67e1bf50ea9dba87180891cbdc
margaretnjoki <maggykiroko@gmail.com>
2026-07-05 12:39:02 +0300

    Every book with its author's name and country, sorted by country.

--- day3_joins.sql
+++ day3_joins.sql
@@ -1,3 +1,10 @@
-SELECT books.title,authors.name
+--
+-- SELECT books.title,authors.name
+-- FROM books
+-- JOIN authors ON books.author_id=authors.id;
+
+
+SELECT books.title,authors.name,authors.country
 FROM books
-JOIN authors ON books.author_id=authors.id;
+JOIN authors ON books.author_id=authors.id
+  ORDER BY country;
