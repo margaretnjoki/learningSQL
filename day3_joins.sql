7ccb6b4e579c4d9245a708443b22b96402b2d6dc
margaretnjoki <maggykiroko@gmail.com>
2026-07-05 18:49:28 +0300

    ustomers who have never placed an order

--- day3_joins.sql
+++ day3_joins.sql
@@ -7,14 +7,27 @@
 -- FROM books
 -- JOIN authors ON books.author_id=authors.id
 --   ORDER BY country;
-SELECT customers.id,
-       customers.name,
-       orders.id AS order_id,
-       orders.status
-FROM customers
-LEFT JOIN orders
-ON customers.id = orders.customer_id;
+-- SELECT customers.id,
+--        customers.name,
+--        orders.id AS order_id,
+--        orders.status
+-- FROM customers
+-- LEFT JOIN orders
+-- ON customers.id = orders.customer_id;
 
+-- Customers who have never placed an order (name only).
+-- SELECT c.name
+-- FROM customers c
+-- JOIN orders ON c.id = orders.customer_id
+--  WHERE NOT EXISTS (SELECT 1 from orders WHERE orders.id = customer_id )
+
+SELECT c.name
+FROM customers c
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM orders o
+    WHERE o.customer_id = c.id
+);
 
 
 
