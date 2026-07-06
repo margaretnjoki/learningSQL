98f9dd9577dd86282187d9cf843d22f58ea716d4
margaretnjoki <maggykiroko@gmail.com>
2026-07-06 13:19:56 +0300

    altering table customers and adding a UNIQUE constraint on customer.email

--- day4_modelling.sql
+++ day4_modelling.sql
@@ -2,7 +2,10 @@
 -- SET price=price + (price * 0.05)
 -- WHERE genre='Programming';
 -- SELECT * FROM BOOKS;//confirming whether it has modified the database.
+
 -- UPDATE orders
 -- SET status='PAID' WHERE id=4;
-DELETE FROM order_items WHERE quantity=0;
 
+-- DELETE FROM order_items WHERE quantity=0;
+
+ALTER TABLE customers ADD CONSTRAINT uq_email UNIQUE (email);
