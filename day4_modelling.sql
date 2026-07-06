48ac1f0174eb45ebd83877699331f6ed1f474bb4
margaretnjoki <maggykiroko@gmail.com>
2026-07-06 13:02:55 +0300

    Mark order #4 as 'PAID'

--- day4_modelling.sql
+++ day4_modelling.sql
@@ -1,5 +1,6 @@
-UPDATE books
-SET price=price + (price * 0.05)
-WHERE genre='Programming';
-
--- SELECT * FROM BOOKS;
+-- UPDATE books
+-- SET price=price + (price * 0.05)
+-- WHERE genre='Programming';
+-- SELECT * FROM BOOKS;//confirming whether it has modified the database.
+UPDATE orders
+SET status='PAID' WHERE id=4;
